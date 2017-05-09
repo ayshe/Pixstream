f8e62342fdf50a6180ee236d802fba04492b5c57
foxgr <grf@fcbtech.com.au>
2017-05-09 14:14:32 +1000

    Added basic data from tutorial

--- index.android.js
+++ index.android.js
@@ -4,50 +4,62 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component,
+} from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
 
 export default class Pixstream extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+      var movie = MOCKED_MOVIES_DATA[0];
+      return (
+              <View style={styles.container}>
+                <Image
+                  source={{uri: movie.posters.thumbnail}}
+                  style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                  <Text style={styles.title}>{movie.title}</Text>
+                  <Text style={styles.year}>{movie.year}</Text>
+                </View>
+              </View>
+            );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+  rightContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+       thumbnail: {
+       width: 53,
+       height: 81,
+     },
+     title: {
+         fontSize: 20,
+         marginBottom: 8,
+         textAlign: 'center',
+       },
+       year: {
+         textAlign: 'center',
+       },
 });
 
 AppRegistry.registerComponent('Pixstream', () => Pixstream);
